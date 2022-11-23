35301a952cbf8358ed80240db9de6fc43292d399
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-11-23 11:06:36 -0300

    feat: criacao das rotas para o CRUD

--- aula_04_intro_laravel/routes/web.php
+++ aula_04_intro_laravel/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProdutoController;
+use Daoo\Aula03\controller\api\Produto;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,3 +32,25 @@ Route::get('/produtos',
 
 Route::get('/produtos/{id}',
     [ProdutoController::class,'show']);
+
+Route::get('/produto',[
+    ProdutoController::class,
+    'create'
+]);
+
+Route::post('/produto',[
+    ProdutoController::class,
+    'store'
+]);
+
+Route::get('/produto/{id}/edit',
+    [ProdutoController::class,'edit'])->name('edit');
+
+Route::post('/produto/{id}/update',
+    [ProdutoController::class,'update'])->name('update');
+
+Route::get('/produto/{id}/delete',
+    [ProdutoController::class,'delete'])->name('delete');
+
+Route::post('/produto/{id}/remove',
+    [ProdutoController::class,'remove'])->name('remove');
