e04f19e447177e5d896fb89f8d7408d4b062439c
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-11-30 10:33:04 -0300

    feat: instalacao do breeze. php artisan:breeze install

--- aula_04_intro_laravel/routes/web.php
+++ aula_04_intro_laravel/routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProdutoController;
-use Daoo\Aula03\controller\api\Produto;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,37 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/ola', function () {
-    return "olá laravel";
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-Route::get('/ola',[HomeController::class,'index']);
-Route::get('/produtos',
-    [ProdutoController::class,'index']);
-
-Route::get('/produtos/{id}',
-    [ProdutoController::class,'show']);
-
-Route::get('/produto',[
-    ProdutoController::class,
-    'create'
-]);
-
-Route::post('/produto',[
-    ProdutoController::class,
-    'store'
-]);
-
-Route::get('/produto/{id}/edit',
-    [ProdutoController::class,'edit'])->name('edit');
-
-Route::post('/produto/{id}/update',
-    [ProdutoController::class,'update'])->name('update');
-
-Route::get('/produto/{id}/delete',
-    [ProdutoController::class,'delete'])->name('delete');
-
-Route::post('/produto/{id}/remove',
-    [ProdutoController::class,'remove'])->name('remove');
+require __DIR__.'/auth.php';
