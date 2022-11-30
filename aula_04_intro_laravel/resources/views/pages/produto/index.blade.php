46b037c66632c5df8ecbd3e392c0c8c50fb64751
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-11-30 09:37:26 -0300

    refact: exemplos da aula 06

--- aula_04_intro_laravel/resources/views/pages/produto/index.blade.php
+++ aula_04_intro_laravel/resources/views/pages/produto/index.blade.php
@@ -1,7 +1,7 @@
 <x-main-layout>
     <h2>Produtos</h2>
     @if ($produtos->count() > 0)
-        <x-tables.produtos :produtos="$produtos" class="table-odd" type="hover" />
+        <x-tables.produtos :produtos="$produtos" class='table-odd' type='hover'/>
         <div style="display:flex; flex-direction: row; justify-content:flex-end">
             <a href="/produto"><button>Criar Novo Produto</button></a>
         </div>
