a9588bea15dc1b6315cc05036ebf4c410bf4bf7a
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-11-30 11:30:57 -0300

    feat: adicionada a tabela de produtos na dashbord

--- aula_04_intro_laravel/resources/views/dashboard.blade.php
+++ aula_04_intro_laravel/resources/views/dashboard.blade.php
@@ -9,7 +9,17 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Produtos") }}
+                    <h2 class='text-4xl'>Produtos</h2>
+                    @if (isset($produtos) && $produtos->count() > 0)
+                        <x-tables.produtos :produtos="$produtos" class='table-odd' type='hover'/>
+                        @auth
+                            <div style="display:flex; flex-direction: row; justify-content:flex-end">
+                                <a href="/produto"><button>Criar Novo Produto</button></a>
+                            </div>
+                        @endauth
+                    @else
+                        <p>Produtos não encontrados! </p>
+                    @endif
                 </div>
             </div>
         </div>
