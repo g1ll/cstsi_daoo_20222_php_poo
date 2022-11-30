649b68dc66032290256b3c60fecc656920d5667c
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-11-30 11:20:58 -0300

    refact: verificando o user autenticado. Diretivas do blade @auth e @endauth

--- aula_04_intro_laravel/resources/views/pages/produto/index.blade.php
+++ aula_04_intro_laravel/resources/views/pages/produto/index.blade.php
@@ -1,10 +1,12 @@
 <x-main-layout>
-    <h2>Produtos</h2>
+    <h2 class='text-4xl'>Produtos</h2>
     @if ($produtos->count() > 0)
         <x-tables.produtos :produtos="$produtos" class='table-odd' type='hover'/>
-        <div style="display:flex; flex-direction: row; justify-content:flex-end">
-            <a href="/produto"><button>Criar Novo Produto</button></a>
-        </div>
+        @auth
+            <div style="display:flex; flex-direction: row; justify-content:flex-end">
+                <a href="/produto"><button>Criar Novo Produto</button></a>
+            </div>
+        @endauth
     @else
         <p>Produtos não encontrados! </p>
     @endif
