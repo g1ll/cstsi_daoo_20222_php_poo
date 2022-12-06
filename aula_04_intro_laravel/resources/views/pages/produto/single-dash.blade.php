b5c9eaa28d35a5073f501bc584d36c9937629ff4
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-12-05 23:07:48 -0300

    feat: componente single para dashboard

---
+++ aula_04_intro_laravel/resources/views/pages/produto/single-dash.blade.php
@@ -0,0 +1,40 @@
+<x-dash-layout>
+<div class="text-center mt-8">
+    @vite('resources/css/show-prod.css')
+    @if ($produto)
+        <h1 class='my-12 text-4xl font-bold'>{{ $produto->nome }}</h1>
+        <p>{{ $produto->descricao }}</p>
+        <table>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>Preço</th>
+                    <td>{{ number_format($produto->preco, 2) }}</td>
+                </tr>
+                <tr>
+                    <th>Quantidade</th>
+                    <td>{{ $produto->qtd_estoque }}</td>
+                </tr>
+                <tr>
+                    <th>Importado</th>
+                    {{-- <td>{{ $produto->importado ? 'Sim' : 'Não' }}</td> --}}
+                    <td>
+                        @if ($produto->importado)
+                            <input type="checkbox" disabled checked>
+                        @else
+                            <input type="checkbox" disabled>
+                        @endif
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <a href="{{ route('edit', $produto->id) }}"><button>editar</button></a>
+        <a href="{{ route('delete', $produto->id) }}"><button>deletar</button></a>
+    @else
+        <p>Produtos não encontrados! </p>
+    @endif
+    <div>
+        <a href="/dashboard">&#9664;Voltar</a>
+    </div>
+</div>
+</x-dash-layout>
