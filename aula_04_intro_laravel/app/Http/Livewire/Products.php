4df960f3ea3e16eaf4c2329efa4068d08c609951
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-12-07 10:06:45 -0300

    feat: metodos de ordenacao para a chamada xhr. wire:click='nomeMetodo' wire:click='orderBy'

--- aula_04_intro_laravel/app/Http/Livewire/Products.php
+++ aula_04_intro_laravel/app/Http/Livewire/Products.php
@@ -2,14 +2,31 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Produto;
 use Livewire\Component;
 
 class Products extends Component
 {
     public $produtos;
+    public $orderAsc=true;
 
     public function render()
     {
         return view('livewire.products');
+    }
+
+    public function orderBy($column='id')
+    {
+        $this->produtos = Produto::orderBy($column, $this->orderAsc ? 'asc' : 'desc')->get();
+        $this->orderAsc = !$this->orderAsc;
+    }
+
+    public function orderByName(){
+        $this->orderBy('nome');
+    }
+    
+    public function orderByPrice()
+    {
+        $this->orderBy('preco');
     }
 }
