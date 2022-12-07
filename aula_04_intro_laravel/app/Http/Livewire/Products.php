b3a0c4d927f4813dca5890c98d66371f33d1221e
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-12-07 11:05:45 -0300

    feat: adapta o live para criacao de novo produto. Cria todos os atributos relacionado com os compos input do form que utilizam wire:model.

--- aula_04_intro_laravel/app/Http/Livewire/Products.php
+++ aula_04_intro_laravel/app/Http/Livewire/Products.php
@@ -3,12 +3,21 @@
 namespace App\Http\Livewire;
 
 use App\Models\Produto;
+use Exception;
 use Livewire\Component;
 
 class Products extends Component
 {
     public $produtos;
     public $orderAsc=true;
+
+    public $nome;
+    public $descricao;
+    public $quantidade;
+    public $preco;
+    public $importado;
+    public $idprod;
+
 
     public function render()
     {
@@ -24,9 +33,40 @@ class Products extends Component
     public function orderByName(){
         $this->orderBy('nome');
     }
-    
+
     public function orderByPrice()
     {
         $this->orderBy('preco');
+    }
+
+    public function save()
+    {
+        $produto = [
+            "nome" => $this->nome,
+            "descricao" => $this->descricao,
+            "preco" => $this->preco,
+            "qtd_estoque" => $this->quantidade,
+            "importado" => $this->importado ? true : false,
+        ];
+
+        // dd($produto);
+
+        try{
+            Produto::create($produto);
+            $this->clear();
+            $this->orderAsc = false;
+            $this->orderBy();
+        }catch(Exception $e){
+            dd('Erro ao inserir');
+        }
+    }
+
+    private function clear()
+    {
+        $this->nome = '';
+        $this->descricao = '';
+        $this->preco = 0;
+        $this->quantidade = 0;
+        $this->importado = null;
     }
 }
