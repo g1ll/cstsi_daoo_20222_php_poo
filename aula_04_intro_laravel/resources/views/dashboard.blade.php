b7ae71b7e8109e70ca710d199a50addd60515bd7
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-12-07 11:08:52 -0300

    refact: adaptacao para redirecionar para a pagina create produto. Não esta relacionado com o livewire.

--- aula_04_intro_laravel/resources/views/dashboard.blade.php
+++ aula_04_intro_laravel/resources/views/dashboard.blade.php
@@ -11,12 +11,11 @@
                 <div class="p-6 text-gray-900">
                     <h2 class='text-4xl'>Produtos</h2>
                     @if (isset($produtos) && $produtos->count() > 0)
+
+                        <div style="display:flex; flex-direction: row; justify-content:flex-end">
+                            <a href="/produto"><button>Criar Novo Produto</button></a>
+                        </div>
                         <x-tables.produtos :produtos="$produtos" class='table-odd' type='hover' />
-                        @auth
-                            <div style="display:flex; flex-direction: row; justify-content:flex-end">
-                                <a href="/produto"><button>Criar Novo Produto</button></a>
-                            </div>
-                        @endauth
                     @else
                         <p>Produtos não encontrados! </p>
                     @endif
