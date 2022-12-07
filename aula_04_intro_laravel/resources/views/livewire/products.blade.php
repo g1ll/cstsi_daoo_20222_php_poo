7591961c48b199481dfcb23e99065b5839a7e810
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-12-07 09:28:17 -0300

    refact: criacao do conteudo do componente livewire

--- aula_04_intro_laravel/resources/views/livewire/products.blade.php
+++ aula_04_intro_laravel/resources/views/livewire/products.blade.php
@@ -1,3 +1,12 @@
 <div>
-    {{-- The whole world belongs to you. --}}
+    @if (isset($produtos) && $produtos->count() > 0)
+        <x-tables.produtos :produtos="$produtos" class='table-odd' type='hover' />
+        @auth
+            <div style="display:flex; flex-direction: row; justify-content:flex-end">
+                <a href="/produto"><button>Criar Novo Produto</button></a>
+            </div>
+        @endauth
+    @else
+        <p>Produtos não encontrados! </p>
+    @endif
 </div>
