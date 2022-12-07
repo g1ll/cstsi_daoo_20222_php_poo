d728b29d88836c0ab50dbac4520fca8be06cbf9a
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-12-07 10:07:32 -0300

    feat: criacao de novo componente de tabela live. Tabela com chamadas ao livewire atraves da diretiva wire:click.

---
+++ aula_04_intro_laravel/resources/views/components/tables/products-live.blade.php
@@ -0,0 +1,40 @@
+<table {{ $attributes->merge(['class' => 'table table-' . $type]) }}>
+    @vite('resources/css/table.css')
+    <thead>
+        <tr>
+            <th><a href="#" wire:click='orderBy'>Id</a></th>
+            <th><a href="#" wire:click='orderByName'>Nome</a></th>
+            <th>qtd_estoque</th>
+            <th><a href="#" wire:click='orderByPrice'>Preco</a></th>
+            <th>Importado</th>
+            @if(Auth::user() && Route::is('dashboard'))
+                <th>Acoes</th>
+            @endif
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($produtos as $produto)
+            <tr>
+                @if(Auth::user() && Route::is('dashboard'))
+                     <td><a href="{{route('produto.single-dash',$produto->id) }}">{{ $produto->id }}</a></td>
+                     <td><a href="{{route('produto.single-dash',$produto->id) }}">{{ $produto->nome }}</a></td>
+                @else
+                    <td><a href="/produtos/{{ $produto->id }}">{{ $produto->id }}</a></td>
+                    <td><a href="/produtos/{{ $produto->id }}">{{ $produto->nome }}</a></td>
+                @endif
+
+                <td>{{ $produto->qtd_estoque }}</td>
+                <td>{{ $produto->preco }}</td>
+                {{-- <td>{{($produto->importado)?'Sim':'Não'}}</td> --}}
+                <td align="center">
+                    <input type="checkbox" disabled {{ $produto->importado ? 'checked' : '' }}>
+                </td>
+                @if(Auth::user() && Route::is('dashboard'))
+                    <td><a href="{{ route('edit', $produto->id) }}">editar</a> |
+                        <a href="{{ route('delete', $produto->id) }}">deletar</a>
+                    </td>
+                @endif
+            </tr>
+        @endforeach
+    </tbody>
+</table>
