12e2851ff1c22850ab2adc13181d33d7b6ddf710
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-12-13 10:15:55 -0300

    fix: alteracoes de estilos com classes tailwind

--- aula_04_intro_laravel/resources/views/livewire/products.blade.php
+++ aula_04_intro_laravel/resources/views/livewire/products.blade.php
@@ -1,6 +1,6 @@
 <div x-data="{ open: false }" class='flex justify-center'>
     <x-modals.forms.produto-create />
-    <div class="w-3/4">
+    <div class="w-full lg:w-5/6">
         <div class="py-3 pr-5 flex justify-start">
             <x-primary-button @click="open = true">Novo Produto</x-primary-button>
         </div>
