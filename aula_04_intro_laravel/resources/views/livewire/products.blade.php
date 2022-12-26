27878059cf7e52301dfd1b0130dbb2fbfc28ed14
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-12-26 09:25:25 -0300

    feat: suporte a relacao fornecedor. Uso da lista de fornecedores, propagação da propriedade para componentes

--- aula_04_intro_laravel/resources/views/livewire/products.blade.php
+++ aula_04_intro_laravel/resources/views/livewire/products.blade.php
@@ -1,11 +1,11 @@
 <div x-data="{ open: false }" class='flex justify-center'>
-    <x-modals.forms.produto-create />
+    <x-modals.forms.produto-create :fornecedores="$listFornecedores"/>
     <div class="w-full lg:w-5/6">
         <div class="py-3 pr-5 flex justify-start">
             <x-primary-button @click="open = true">Novo Produto</x-primary-button>
         </div>
-        @if (isset($produtos) && $produtos->count() > 0)
-            <x-tables.products-live :produtos="$produtos" class='table-odd' type='hover' />
+        @if (isset($listProdutos) && count($listProdutos)>0)
+            <x-tables.products-live :produtos="$listProdutos" :fornecedores="$listFornecedores" class='table-odd' type='hover' />
         @else
             <p>Produtos não encontrados! </p>
         @endif
