3070a3ab54e89fde254b4fb761f6c6b60f838a78
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-12-26 09:24:01 -0300

    refact: alteracao da propriedade  lista  produtos. Refatoração do código da classe do livewire e alteracao de variaveis.

--- aula_04_intro_laravel/resources/views/pages/produto/index.blade.php
+++ aula_04_intro_laravel/resources/views/pages/produto/index.blade.php
@@ -1,4 +1,4 @@
 <x-dash-layout>
     <h2 class='text-4xl text-center py-2'>Produtos</h2>
-    <livewire:products :produtos="$produtos">
+    <livewire:products :listProdutos="$produtos">
 </x-dash-layout>
