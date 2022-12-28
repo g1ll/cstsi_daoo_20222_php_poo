dfe85b2e4d0f03dcb1480cdc61cda510a5e21c18
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-12-27 23:13:46 -0300

    refact: repasse dos items de paginacao

--- aula_04_intro_laravel/resources/views/pages/produto/index.blade.php
+++ aula_04_intro_laravel/resources/views/pages/produto/index.blade.php
@@ -1,4 +1,4 @@
 <x-dash-layout>
     <h2 class='text-4xl text-center py-2'>Produtos</h2>
-    <livewire:products :listProdutos="$produtos">
+    <livewire:products :listProdutos="collect($produtos->items())"/>
 </x-dash-layout>
