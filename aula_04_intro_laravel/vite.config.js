bebfe8ec7211e2e0f6610477c2c58ed690c23c6e
Ryzen from R900 <echos999@gmail.com>
2024-05-20 20:07:37 -0300

    chore: added options to expose dev server

--- aula_04_intro_laravel/vite.config.js
+++ aula_04_intro_laravel/vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/css/show-prod.css',
-                'resources/css/show-prod-green.css',
-                'resources/css/table.css'
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/show-prod.css",
+                "resources/css/show-prod-green.css",
+                "resources/css/table.css",
             ],
             refresh: true,
         }),
     ],
+    server: {
+        host: true,
+    },
 });
