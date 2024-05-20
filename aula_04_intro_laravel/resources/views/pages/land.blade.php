d8dd6a21deb27d121a1b9fe8b25519a28d98cd3e
Ryzen from R900 <echos999@gmail.com>
2024-05-20 20:32:13 -0300

    fix: update component tag and fixes styles

--- aula_04_intro_laravel/resources/views/pages/land.blade.php
+++ aula_04_intro_laravel/resources/views/pages/land.blade.php
@@ -1,9 +1,9 @@
 <x-main-layout>
     <div class="w-full 2xl:max-w-screen-xl">
-        <div class="grid w-fit h-fit min-h-80 gap-x-8 2xl:grid-cols-5 2xl:gap-8 md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 gap-y-5">
+        <div class="grid mx-auto sm:w-fit h-fit min-h-80 gap-x-8 2xl:grid-cols-5 2xl:gap-8 md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 gap-y-5">
             @if (isset($produtos) && $produtos->count() > 0)
                 @foreach ($produtos as $produto)
-                    <x-cards.produtos :produto="$produto" />
+                    <x-cards.produtos-card :$produto />
                 @endforeach
             @endif
         </div>
