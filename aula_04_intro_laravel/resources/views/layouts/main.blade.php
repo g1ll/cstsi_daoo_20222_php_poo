eac4c1a02cb65177064a4a7ddd60330bca4c3e23
Ryzen from R900 <echos999@gmail.com>
2024-05-20 21:18:51 -0300

    style: fixes padding for 320px screen

--- aula_04_intro_laravel/resources/views/layouts/main.blade.php
+++ aula_04_intro_laravel/resources/views/layouts/main.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-navbar/>
     <x-header />
-    <main class='flex justify-center w-full p-10 '>
+    <main class='flex justify-center w-full p-3 sm:p-10 '>
         {{$slot}}
     </main>
 </x-guest-layout>
